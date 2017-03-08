64089334fdcf17011135a3679c9b832bcfccc500
SunTiger1984 <jennispaghetti@hotmail.co.uk>
2017-03-08 21:16:08 +0000

    Played with javascript 1

---
+++ script.js
@@ -0,0 +1,21 @@
+function clicked() {
+    $(this).css("background-color", "red")
+}
+
+
+
+  $(document).ready(function(){
+
+    var button = $("#submitemail");
+
+    console.log("got the buttons: " + button)
+
+    button.on('click', clicked)
+
+    button.on('click', function(){
+      $(this).html("Submitted")
+    })
+
+
+
+  })
